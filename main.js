87a55dd758e2daf6ed15f516ba7e2a9eb2f9b54f
Patt Sookmark <pijiya@sookmark.com>
2022-10-29 21:17:04 -0600

    Adding script for radio buttons

--- main.js
+++ main.js
@@ -12,3 +12,31 @@ var cookBtn = document.querySelector('#cook')
 var foodItem = document.getElementById('food-item')
 var showFoodSl = document.querySelector('.food-idea')
 var crocpotBox = document.querySelector('.svg')
+
+//eventslisterner 
+
+//when click button with chosen radio, it randomly select things in the radio array, and display the text in the crocpot box
+cookBtn.addEventListener('click',function(){
+    // swap();
+    letsCook();
+ })
+
+ function letsCook(){
+    var sideId = getRandomIndex(sideDish)
+    var mainId = getRandomIndex(mainDish)
+    var dessertId = getRandomIndex(dessertDish)
+ 
+    if(slSide.checked === true){
+       foodItem.innerText = sideDish[sideId];
+       console.log(sideDish[sideId])
+       }else if(slMain.checked === true){
+          foodItem.innerText = mainDish[mainId];
+       }else if(slDessert.checked === true){
+          foodItem.innerText = dessertDish[dessertId];
+    } 
+}
+
+//function
+function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length)
+ }
