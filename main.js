4b6f475f984bb30f47934d80a085c81af5c393ee
Patt Sookmark <pijiya@sookmark.com>
2022-10-29 21:11:16 -0600

    adding variables and queryselect

---
+++ main.js
@@ -0,0 +1,14 @@
+//Variables
+var sideDish = ['Mac and Cheese','Fries','Coleslaw','Potato Salad','Onion Rings']
+var mainDish = ['Wings','Cherry Cricket Burger','Bison Burger','Turkey Burger','Enchilada']
+var dessertDish = ['Brownies','Milkshake','Peach Crumble','Donuts']
+
+//querySelect
+var slSide = document.querySelector('#sideSl')
+var slMain = document.querySelector('#mainSL')
+var slDessert = document.querySelector('#dessertSL')
+var slMeal = document.querySelector('#mealSL')
+var cookBtn = document.querySelector('#cook')
+var foodItem = document.getElementById('food-item')
+var showFoodSl = document.querySelector('.food-idea')
+var crocpotBox = document.querySelector('.svg')
